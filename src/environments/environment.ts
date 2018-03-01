3da6a1dd31c6b2018b8992e3ff27698344873c29
Decebel Geduriagao <Decebel Geduriagao>
2018-03-01 19:41:41 +0800

    added firebase cred

---
+++ src/environments/environment.ts
@@ -0,0 +1,18 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig:{
+    apiKey: "AIzaSyDjbvVfiUrXku_SZxr6EeotTeh0L0920jk",
+    authDomain: "angular-crypto-currency-da303.firebaseapp.com",
+    databaseURL: "https://angular-crypto-currency-da303.firebaseio.com",
+    projectId: "angular-crypto-currency-da303",
+    storageBucket: "angular-crypto-currency-da303.appspot.com",
+    messagingSenderId: "326714318256"
+
+  }
+
+};
